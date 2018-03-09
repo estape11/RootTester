1a81901296f305bd4872e25dddf6d1e5d921f7c3
estape11 <estape11@gmail.com>
2018-03-09 02:48:08 -0600

    Update

--- include/RootBrent.hpp
+++ include/RootBrent.hpp
@@ -18,37 +18,53 @@
 #define ANPI_ROOT_BRENT_HPP
 
 namespace anpi {
-  
-  /**
-   * Find the roots of the function funct looking for it in the
-   * interval [xl,xu], using the Brent's method.
-   *
-   * @param funct a std::function of the form "T funct(T x)"
-   * @param xl lower interval limit
-   * @param xu upper interval limit
-   *
-   * @return root found, or NaN if none could be found.
-   *
-   * @throws anpi::Exception if inteval is reversed or both extremes
-   *         have same sign.
-   */
-  template<typename T>
-  T rootBrent(const std::function<T(T)>& funct,T xl,T xu,const T eps = sqrt(std::numeric_limits<T>::epsilon())) {
-    T fl=funct(xl);
-    T fu=funct(xu);
-    if (stb::abs(fl)<std::abs(fu)){
-      T xSwap=T(xl);
-      xl=T(xu);
-      xu=T(xSwap);
+
+    /**
+     * Find the roots of the function funct looking for it in the
+     * interval [xl,xu], using the Brent's method.
+     *
+     * @param funct a std::function of the form "T funct(T x)"
+     * @param xl lower interval limit
+     * @param xu upper interval limit
+     *
+     * @return root found, or NaN if none could be found.
+     *
+     * @throws anpi::Exception if inteval is reversed or both extremes
+     *         have same sign.
+     */
+    template<typename T>
+    T rootBrent(const std::function<T(T)> &funct, T xl, T xu, const T eps = sqrt(std::numeric_limits<T>::epsilon())) {
+        // xl=a xu=b
+        T fl = funct(xl);
+        T fu = funct(xu);
+        if (stb::abs(fl) < std::abs(fu)) {
+            T xSwap = T(xl);
+            xl = T(xu);
+            xu = T(xSwap);
+            fl = funct(xl);
+            fu = funct(xu);
+        }
+        T c = T(xl);
+        T fc = funct(c);
+        T xs = T(0);
+        bool mflag = true;
+        for (int i = maxi; i > 0; i--) {
+            if (fl != fc && fu != fc) {
+                xs = (xl * fu * fc) / ((fl - fu) * (fl - fc)) + (xu * fl * fc) / ((fu - fl) * (fu - fc)) +
+                     (c * fl * fc) / ((fc - fl) * (fc - fu));
+            } else {
+                xs = b - fu * (xu - xl) / (fu - fl);
+            }
+            if (!((3 * xl + b) / 4 < xs < xu) ||
+                    (mflag && std::abd(xs-xu)>=std::abs(b-c)/2)){
+
+            }
+        }
+
+        // Return NaN if no root was found
+        return std::numeric_limits<T>::quiet_NaN();
     }
-    T c=T(xu);
-    bool mflag=true;
-
-
-    // Return NaN if no root was found
-    return std::numeric_limits<T>::quiet_NaN();
-  }
 }
-  
+
 #endif
 
